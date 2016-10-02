07b0b7ea08b008b246773078016efc42e95a003f
Vitalik <Vitalik.Trofimenko.1998@mail.ru>
2016-10-02 16:43:24 +0700

    Add files via upload

---
+++ Program5.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a;
+            Console.WriteLine("Введите число ");
+            a = double.Parse(Console.ReadLine());
+
+            if (a % 1 == 0)
+            {
+                Console.WriteLine("Ваше число является целым");
+            }
+            else
+            {
+                Console.WriteLine("Ваше число не является целым");
+            }
+           
+            Console.ReadKey();
+        }
+    }
+}
