bd9db76fb2aac9ba6ef5dbcff9343850e80c3d60
Vitalik <Vitalik.Trofimenko.1998@mail.ru>
2016-10-02 16:49:54 +0700

    Add files via upload

---
+++ Program2.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, c, d;
+            Console.WriteLine("Введите координаты вектора A : ");
+            a = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Введите координаты вектора B : ");
+            b = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Введите координаты вектора C : ");
+            c = int.Parse(Console.ReadLine());
+
+            d=(a*a)+(b*b)+(c*c);
+
+            Console.WriteLine("Длина вектора состовляет: "+d);
+
+            Console.ReadKey();
+        }
+    }
+}
