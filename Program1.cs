0a8b97ed1c5ce2149d48b30abe4c9907ea3928af
Vitalik <Vitalik.Trofimenko.1998@mail.ru>
2016-10-02 16:50:48 +0700

    Add files via upload

---
+++ Program1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, c;
+            Console.WriteLine("Ввведите число :");
+            a = int.Parse(Console.ReadLine());
+
+            b = a / 10;
+            c = a % 10;
+            if (c == b)
+            {
+                Console.WriteLine(c + " Ни равны " + b);
+            }
+            else if (b > c)
+            {
+                Console.WriteLine(b + " Больше " + c);
+            }
+            else
+                Console.WriteLine(c + " Больше " + b);
+            Console.ReadKey();
+        }
+    }
+}
